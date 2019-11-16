66604c52c37ff6d91a5f43d477473894b8cc9c78
cuijie <648917993@qq.com>
2019-11-16 10:15:26 +0800

    查看页面后提交

---
+++ cloud-parent/client-config/src/main/java/com/cuijie/demo/ClientConfigApplication.java
@@ -0,0 +1,12 @@
+package com.cuijie.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientConfigApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(ClientConfigApplication.class,args);
+    }
+}
---
+++ cloud-parent/client-config/src/main/java/com/cuijie/demo/controller/ShowControlelr.java
@@ -0,0 +1,23 @@
+package com.cuijie.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.print.DocFlavor;
+
+@RestController
+@RequestMapping("/show")
+public class ShowControlelr {
+
+    @Value("${env_name}")
+    private String envName;
+    @RequestMapping("/env")
+    public String showEnv(){
+
+        return envName;
+
+    }
+
+}
